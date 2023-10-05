a2302a7a10fe7d1b058aa990fdb03b54e5f24799
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-10-05 01:07:58 -0300

    created db.json to use with json server; changed app.tsx an main

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Topic } from "./interfaces/topic";
+import { Header } from "./components/Header";
+import { TopicList } from "./components/TopicList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [topics, setTopics] = useState<Topic[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      
+      const response = await fetch('http://localhost:3000/topics')
+
+      const data = await response.json();
+      const topics: Topic[] = data as Topic[];
+      topics.forEach((topic) => (topic.createdAt = new Date(topic.createdAt)));
+
+      const sortedTopics = topics.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+      setTopics(sortedTopics);
+    })();
+  }, []);
+
+  const onHandleChangeActive = async (topic: Topic) => {
+    topic.active = !topic.active;
+    const response = await fetch(`http://localhost:3000/topics/${topic.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(topic),
+    });
+
+    if (response.ok) {
+      const data: Topic = await response.json();
+      const updatedTopics = topics.map((currentTopic) => (currentTopic.id === data.id ? data : currentTopic));
+      setTopics(updatedTopics);
+    }
+  };
+
+  const onHandleLikeTopic = async (topic: Topic) => {
+    topic.likes += 1;
+    const response = await fetch(`http://localhost:3000/topics/${topic.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(topic),
+    });
+
+    if (response.ok) {
+      const data: Topic = await response.json();
+      const updatedTopics = topics.map((currentTopic) => (currentTopic.id === data.id ? data : currentTopic));
+      setTopics(updatedTopics);
+    }
+  };
+  const onHandleDislikeTopic = async (topic: Topic) => {
+    topic.dislikes += 1;
+    const response = await fetch(`http://localhost:3000/topics/${topic.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(topic),
+    });
+
+    if (response.ok) {
+      const data: Topic = await response.json();
+      const updatedTopics = topics.map((currentTopic) => (currentTopic.id === data.id ? data : currentTopic));
+      setTopics(updatedTopics);
+    }
+  };
+
+  const onCreateTopic = async (topic: Topic) => {
+    const response = await fetch("http://localhost:3000/topics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(topic),
+    });
+
+    if (response.ok) {
+      setTopics([topic, ...topics]);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header onCreateTopic={onCreateTopic} />
+      <TopicList topics={topics} changeActive={onHandleChangeActive} likeTopic={onHandleLikeTopic} dislikeTopic={onHandleDislikeTopic} />
     </>
-  )
+  );
 }
-
-export default App
+export default App;
